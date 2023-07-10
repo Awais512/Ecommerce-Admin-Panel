2a8cfac7fac4b36b825734d0d51a20eaefe1c32c
awais512 <raza.awais25@gmail.com>
2023-07-10 23:18:25 +0100

    Modal Store Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="p-4">
       <Modal isOpen onClose={() => {}} title="Test" description="Test Desc">
-        Children
+        Test
       </Modal>
     </div>
   );

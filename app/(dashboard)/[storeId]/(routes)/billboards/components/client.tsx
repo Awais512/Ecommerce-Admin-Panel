8a880b7f299a79596ec6d31b50ecfb0b29b8259a
awais512 <raza.awais25@gmail.com>
2023-08-29 19:31:54 +0100

    Billboard Form 1

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,9 +3,13 @@ import Heading from "@/components/ui/Heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { PlusIcon } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import React from "react";
 
 const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,7 +17,9 @@ const BillboardClient = () => {
           title="Billboards(0)"
           description="Manage billboards of your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <PlusIcon className="mr-2 w-4 h-4" />
           Add New
         </Button>
